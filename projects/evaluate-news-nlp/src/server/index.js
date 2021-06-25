962902431b38dfdd3a489bcda585a1560da6f554
madomb01 <madomb01@gmail.com>
2021-06-24 22:43:57 -0400

    Attempt 2
    Based on changes recommended in https://knowledge.udacity.com/questions/612607

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,32 +1,52 @@
-var dotenv = require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
-
-// You could call it aylienapi, or anything else
-var textapi = process.env.API_KEY;
-// });
-
-var baseURL = "https://api.meaningcloud.com/sentiment-2.1";
-var path = require('path')
+const APIKey = process.env.API_KEY;
+console.log("MeaningCloud APPI key =", APIKey)
+let path = require('path')
 const express = require('express')
+const fetch = require('node-fetch')
 const mockAPIResponse = require('./mockAPI.js')
-
+const bodyParser = require('body-parser');
+const cors = require('cors')
+let json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
 const app = express()
-
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(express.static('dist'))
-
-console.log(__dirname)
-
+console.log(JSON.stringify(mockAPIResponse))
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
-
+app.get('/test', function (req, res) {
+    res.json(mockAPIResponse);
+})
+// Setup Server
+const port = 8080;
+// Spin up the server
+// const server = app.listen(port, () => console.log(`Running on localhost: ${port}`));
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
+app.listen(port, function () {
     console.log('Example app listening on port 8080!')
 })
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// POST route
+app.post('/inputfield', async (req, res) => {
+    console.log('req====+>', req.body)
+    const result = await fetch("https://api.meaningcloud.com/sentiment-2.1?key=" + process.env.API_KEY + "&url=" + req.body.url + "&lang=en")
+    try {
+        console.log(result)
+        const response = await result.json();
+        res.send(response)
+        console.log(response)
+    } catch (error) {
+        console.log("error", error);
+    }
+});
